20b603afa23f12447e2f12e76a9b2ee56f7cdd28
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-31 19:18:17 -0300

    Create footer copmonent

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledFooter } from "./style";
+const Footer = () => {
+  return (
+    <StyledFooter id="footer">
+      <small>Jezreel de Andrade - 2023 &copy;</small>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.section`
+  height: 50px;
+  width: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
