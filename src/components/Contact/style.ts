14fd173a00e807face6bd49ff4929284bf7cd577
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-31 13:25:27 -0300

    Create contact style

---
+++ src/components/Contact/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledContact = styled.div`
+  h3 {
+    color: cyan;
+  }
+`;
+
+export const SocialMedia = styled.ul`
+  list-style-type: none;
+  color: #000;
+  display: grid;
+  grid-gap: 10px 10px;
+  grid-template-columns: repeat(4, 1fr);
+`;
