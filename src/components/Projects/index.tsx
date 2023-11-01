5d0c31d964ec900564e23059d985f4f5b7ff58bd
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-31 21:18:27 -0300

    Add SideNav

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledProjects } from "./style";
+
+const Projects = () => {
+  return (
+    <StyledProjects id="projects">
+      <h3>Projects</h3>
+    </StyledProjects>
+  );
+};
+
+export default Projects;
---
+++ src/components/Projects/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledProjects = styled.section`
+  height: 700px;
+  width: inherit;
+  text-align: center;
+`;
