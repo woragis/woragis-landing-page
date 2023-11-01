9cb45f59d681633adc97d1118859c63121c6836b
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-31 21:17:54 -0300

    Create about component

---
+++ src/components/About/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyledAbout } from "./style";
+
+const About = () => {
+  return (
+    <StyledAbout id="about-me">
+      <h3>
+        About <span>Jezreel de Andrade</span>
+      </h3>
+      <p>
+        Autodidata, fluente em ingles, programador, perde noites e feriados pelo
+        projeto, pois programar /e um hobbie e nao um trabalho
+      </p>
+    </StyledAbout>
+  );
+};
+
+export default About;
---
+++ src/components/About/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledAbout = styled.section`
+  height: 500px;
+  width: inherit;
+`;
